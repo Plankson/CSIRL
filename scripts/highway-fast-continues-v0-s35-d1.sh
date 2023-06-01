07b3f28717760b8fd0be28a83e4ee39d6cce7ae8
Shunyu <30792664+liushunyu@users.noreply.github.com>
2023-06-01 13:18:36 +0800

    Update highway-fast-continues-v0-s35-d1.sh

--- scripts/highway-fast-continues-v0-s35-d1.sh
+++ scripts/highway-fast-continues-v0-s35-d1.sh
@@ -1,7 +1,7 @@
 
-python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="/expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=0
-python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="/expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=1
-python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="/expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=2
-python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="/expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=3
-python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="/expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=4
-python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="/expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=5
+python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=0
+python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=1
+python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=2
+python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=3
+python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=4
+python main.py env=highway-fast-continues-v0_s35_d1  expert.tra="expert_data/highway-fast-continues-v0-s35-d1/25.npy" seed=5
