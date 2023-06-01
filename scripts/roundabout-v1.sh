db41d566ca15b4d0984552cf3fa3f9e02462bc3b
Shunyu <30792664+liushunyu@users.noreply.github.com>
2023-06-01 13:20:41 +0800

    Update roundabout-v1.sh

--- scripts/roundabout-v1.sh
+++ scripts/roundabout-v1.sh
@@ -1,39 +1,41 @@
-#task 1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/25.npy" seed=0
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/25.npy" seed=1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/25.npy" seed=2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/25.npy" seed=3
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/25.npy" seed=4
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/25.npy" seed=5
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/25.npy" seed=0
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/25.npy" seed=1
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/25.npy" seed=2
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/25.npy" seed=3
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/25.npy" seed=4
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/25.npy" seed=5
 
-#task 2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/1.npy" seed=0
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/1.npy" seed=1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/1.npy" seed=2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/1.npy" seed=3
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/1.npy" seed=4
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/1.npy" seed=5
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/5.npy" seed=0
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/5.npy" seed=1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/5.npy" seed=2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/5.npy" seed=3
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/5.npy" seed=4
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/5.npy" seed=5
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/10.npy" seed=0
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/10.npy" seed=1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/10.npy" seed=2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/10.npy" seed=3
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/10.npy" seed=4
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/10.npy" seed=5
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/15.npy" seed=0
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/15.npy" seed=1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/15.npy" seed=2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/15.npy" seed=3
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/15.npy" seed=4
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/15.npy" seed=5
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/20.npy" seed=0
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/20.npy" seed=1
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/20.npy" seed=2
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/20.npy" seed=3
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/20.npy" seed=4
-python main.py env=roundabout_continues_v1 expert.tra="/expert_data/roundabout-continues-v1/20.npy" seed=5
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/1.npy" seed=0
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/1.npy" seed=1
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/1.npy" seed=2
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/1.npy" seed=3
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/1.npy" seed=4
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/1.npy" seed=5
+
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/5.npy" seed=0
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/5.npy" seed=1
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/5.npy" seed=2
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/5.npy" seed=3
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/5.npy" seed=4
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/5.npy" seed=5
+
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/10.npy" seed=0
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/10.npy" seed=1
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/10.npy" seed=2
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/10.npy" seed=3
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/10.npy" seed=4
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/10.npy" seed=5
+
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/15.npy" seed=0
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/15.npy" seed=1
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/15.npy" seed=2
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/15.npy" seed=3
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/15.npy" seed=4
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/15.npy" seed=5
+
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/20.npy" seed=0
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/20.npy" seed=1
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/20.npy" seed=2
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/20.npy" seed=3
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/20.npy" seed=4
+python main.py env=roundabout_continues_v1 expert.tra="expert_data/roundabout-continues-v1/20.npy" seed=5
