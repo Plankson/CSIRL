dfde0ce35802e445b0725b8e06a7311da0a8e3b5
Shunyu <30792664+liushunyu@users.noreply.github.com>
2023-06-01 13:19:58 +0800

    Update merge-v0.sh

--- scripts/merge-v0.sh
+++ scripts/merge-v0.sh
@@ -1,7 +1,7 @@
 
-python main.py env=merge-continues-v0  expert.tra="/expert_data/merge-continues-v0/25.npy" seed=0
-python main.py env=merge-continues-v0  expert.tra="/expert_data/merge-continues-v0/25.npy" seed=1
-python main.py env=merge-continues-v0  expert.tra="/expert_data/merge-continues-v0/25.npy" seed=2
-python main.py env=merge-continues-v0  expert.tra="/expert_data/merge-continues-v0/25.npy" seed=3
-python main.py env=merge-continues-v0  expert.tra="/expert_data/merge-continues-v0/25.npy" seed=4
-python main.py env=merge-continues-v0  expert.tra="/expert_data/merge-continues-v0/25.npy" seed=5
+python main.py env=merge-continues-v0  expert.tra="expert_data/merge-continues-v0/25.npy" seed=0
+python main.py env=merge-continues-v0  expert.tra="expert_data/merge-continues-v0/25.npy" seed=1
+python main.py env=merge-continues-v0  expert.tra="expert_data/merge-continues-v0/25.npy" seed=2
+python main.py env=merge-continues-v0  expert.tra="expert_data/merge-continues-v0/25.npy" seed=3
+python main.py env=merge-continues-v0  expert.tra="expert_data/merge-continues-v0/25.npy" seed=4
+python main.py env=merge-continues-v0  expert.tra="expert_data/merge-continues-v0/25.npy" seed=5
